addd8e375503959c87cdf20d9b5902fe207e48f7
Pankaj Kumar <64430912+Pankaj2612@users.noreply.github.com>
2023-01-27 22:53:07 +0530

    q6 to q8

---
+++ Sem 1/Assignment_5/src/q9.java
@@ -0,0 +1,10 @@
+// Write a program that will read the value of n from the user and calculate sum of the following
+// series:
+
+public class q9 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
