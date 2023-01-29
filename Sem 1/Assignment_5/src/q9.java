d7fdc47f6cf0c73c614ac2808c84258a75968ebc
Pankaj Kumar <64430912+Pankaj2612@users.noreply.github.com>
2023-01-29 20:04:24 +0530

    q9

--- Sem 1/Assignment_5/src/q9.java
+++ Sem 1/Assignment_5/src/q9.java
@@ -1,9 +1,25 @@
 // Write a program that will read the value of n from the user and calculate sum of the following
 // series:
 
+import java.util.Scanner;
 public class q9 {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of N : ");
+        int n = sc.nextInt();
+
+        double sum = 0;
+        // int div = 1;
+
+        for (int i =1;i <=n ; i++){
+
+            sum = sum + 1/(Math.pow(i, 2));
+
+        }
+        System.out.println("The sum of the Series is : " + sum);
+
         
     }
     
